7a1f5a733073515d7f8287f2dc5cba62f77fd939
Valentin Staykov <v.staykov@razorlabs.com>
2024-03-02 15:46:38 +0000

    better naming

--- rpc-match-blockhashes/main.go
+++ rpc-match-blockhashes/main.go
@@ -87,61 +87,111 @@ func main() {
 
 		// check all fields
 		if blockA.ParentHash() != blockB.ParentHash() {
-			log.Warn("ParentHash", "Rpc", blockA.ParentHash().Hex(), "Rpc", blockB.ParentHash().Hex())
+			log.Warn("ParentHash", "A", blockA.ParentHash().Hex(), "B", blockB.ParentHash().Hex())
 		}
 		if blockA.UncleHash() != blockB.UncleHash() {
-			log.Warn("UnclesHash", "Rpc", blockA.UncleHash().Hex(), "Local", blockB.UncleHash().Hex())
+			log.Warn("UnclesHash", "A", blockA.UncleHash().Hex(), "B", blockB.UncleHash().Hex())
 		}
 		if blockA.Root() != blockB.Root() {
-			log.Warn("Root", "Rpc", blockA.Root().Hex(), "Local", blockB.Root().Hex())
+			log.Warn("Root", "A", blockA.Root().Hex(), "B", blockB.Root().Hex())
 		}
 		if blockA.TxHash() != blockB.TxHash() {
-			log.Warn("TxHash", "Rpc", blockA.TxHash().Hex(), "Local", blockB.TxHash().Hex())
+			log.Warn("TxHash", "A", blockA.TxHash().Hex(), "B", blockB.TxHash().Hex())
+		}
+
+		txHashesA := make([]common.Hash, len(blockA.Transactions()))
+		for i, tx := range blockA.Transactions() {
+			txHashesA[i] = tx.Hash()
+		}
+		txHashesB := make([]common.Hash, len(blockB.Transactions()))
+		for i, tx := range blockB.Transactions() {
+			txHashesB[i] = tx.Hash()
+		}
+
+		if len(txHashesA) != len(txHashesB) {
+			log.Warn("Transactions amount mismatch", "A", len(txHashesA), "B", len(txHashesB))
+
+			log.Warn("RPAc transactions", "txs", txHashesA)
+			log.Warn("B transactions", "txs", txHashesB)
+		} else {
+			for i, txA := range txHashesB {
+				txB := txHashesB[i]
+				if txA != txB {
+					log.Warn("TxHash", txA.Hex(), txB.Hex())
+				}
+			}
 		}
 
 		if blockA.ReceiptHash() != blockB.ReceiptHash() {
-			log.Warn("ReceiptHash mismatch. Checking receipts", "Rpc", blockA.ReceiptHash().Hex(), "Local", blockB.ReceiptHash().Hex())
-			for _, tx := range blockA.Transactions() {
-				receiptA, receiptB, err := getReceipt(*rpcClientA, *rpcClientB, tx.Hash())
+			log.Warn("ReceiptHash mismatch. Checking receipts", "A", blockA.ReceiptHash().Hex(), "B", blockB.ReceiptHash().Hex())
+			for y, tx := range txHashesA {
+				receiptA, receiptB, err := getReceipt(*rpcClientA, *rpcClientB, tx)
 				if err != nil {
 					log.Error(fmt.Sprintf("getReceipt: %s", err))
 					return
 				}
 				log.Info("-------------------------------------------------")
-				log.Info("Checking Receipts for tx.", "TxHash", tx.Hash().Hex())
+				log.Info("Checking Receipts for tx.", "TxHash", tx.Hex())
 
 				if receiptB.Status != receiptA.Status {
-					log.Warn("ReceiptStatus", "Rpc", receiptA.Status, "Local", receiptB.Status)
+					log.Warn("ReceiptStatus", "A", receiptA.Status, "B", receiptB.Status)
 				}
 				if receiptB.CumulativeGasUsed != receiptA.CumulativeGasUsed {
-					log.Warn("CumulativeGasUsed", "Rpc", receiptA.CumulativeGasUsed, "Local", receiptB.CumulativeGasUsed)
+					log.Warn("CumulativeGasUsed", "A", receiptA.CumulativeGasUsed, "B", receiptB.CumulativeGasUsed)
 				}
 				if !reflect.DeepEqual(receiptB.PostState, receiptA.PostState) {
-					log.Warn("PostState", "Rpc", common.BytesToHash(receiptA.PostState), "Local", common.BytesToHash(receiptB.PostState))
+					log.Warn("PostState", "A", common.BytesToHash(receiptA.PostState), "B", common.BytesToHash(receiptB.PostState))
 				}
 				if receiptB.ContractAddress != receiptA.ContractAddress {
-					log.Warn("ContractAddress", "Rpc", receiptA.ContractAddress, "Local", receiptB.ContractAddress)
+					log.Warn("ContractAddress", "A", receiptA.ContractAddress, "B", receiptB.ContractAddress)
 				}
 				if receiptB.GasUsed != receiptA.GasUsed {
-					log.Warn("GasUsed", "Rpc", receiptA.GasUsed, "Local", receiptB.GasUsed)
+					log.Warn("GasUsed", "A", receiptA.GasUsed, "B", receiptB.GasUsed)
 				}
 				if receiptB.Bloom != receiptA.Bloom {
-					log.Warn("LogsBloom", "Rpc", receiptA.Bloom, "Local", receiptB.Bloom)
+					log.Warn("LogsBloom", "A", receiptA.Bloom, "B", receiptB.Bloom)
 				}
 
-				for i, logA := range receiptA.Logs {
+				if len(receiptA.Logs) != len(receiptB.Logs) {
+					log.Warn("Receipt log amount mismatch", "receipt index", y, "A log amount", len(receiptA.Logs), "B log amount", len(receiptB.Logs))
+
+					logIndexesA := make([]uint, len(receiptA.Logs))
+					for i, log := range receiptA.Logs {
+						logIndexesA[i] = log.Index
+					}
+					logIndexesB := make([]uint, len(receiptB.Logs))
+					for i, log := range receiptB.Logs {
+						logIndexesB[i] = log.Index
+					}
+
+					log.Warn("A log indexes", "indexes", logIndexesA)
+					log.Warn("B log indexes", "indexes", logIndexesB)
+				}
+
+				// still check the available logs
+				// there should be a mismatch on the first index they differ
+				smallerLogLength := len(receiptB.Logs)
+				if len(receiptA.Logs) < len(receiptB.Logs) {
+					smallerLogLength = len(receiptA.Logs)
+				}
+				for i := 0; i < smallerLogLength; i++ {
+
+					log.Warn("-------------------------------------------------")
+					log.Warn("	Checking Logs for receipt.", "index", i)
 					logB := receiptB.Logs[i]
+					logA := receiptA.Logs[i]
+
 					if logA.Address != logB.Address {
-						log.Warn("Log Address", "index", i, "Rpc", logA.Address, "Local", logB.Address)
+						log.Warn("Log Address", "index", i, "A", logA.Address, "B", logB.Address)
 					}
 					if !reflect.DeepEqual(logA.Data, logB.Data) {
-						log.Warn("Log Data", "index", i, "Rpc", logA.Data, "Local", logB.Data)
+						log.Warn("Log Data", "index", i, "A", logA.Data, "B", logB.Data)
 					}
 
 					for j, topicA := range logA.Topics {
 						topicB := logB.Topics[j]
 						if topicA != topicB {
-							log.Warn("Log Topic", "Log index", i, "Topic index", j, "Rpc", topicA, "Local", topicB)
+							log.Warn("Log Topic", "Log index", i, "Topic index", j, "A", topicA, "B", topicB)
 						}
 					}
 				}
@@ -150,31 +200,31 @@ func main() {
 			}
 		}
 		if blockA.Bloom() != blockB.Bloom() {
-			log.Warn("Bloom", "Rpc", blockA.Bloom(), "Local", blockB.Bloom())
+			log.Warn("Bloom", "A", blockA.Bloom(), "B", blockB.Bloom())
 		}
 		if blockA.Difficulty().Cmp(blockB.Difficulty()) != 0 {
-			log.Warn("Difficulty", "Rpc", blockA.Difficulty().Uint64(), "Local", blockB.Difficulty().Uint64())
+			log.Warn("Difficulty", "A", blockA.Difficulty().Uint64(), "B", blockB.Difficulty().Uint64())
 		}
 		if blockA.NumberU64() != blockB.NumberU64() {
-			log.Warn("NumberU64", "Rpc", blockA.NumberU64(), "Local", blockB.NumberU64())
+			log.Warn("NumberU64", "A", blockA.NumberU64(), "B", blockB.NumberU64())
 		}
 		if blockA.GasLimit() != blockB.GasLimit() {
-			log.Warn("GasLimit", "Rpc", blockA.GasLimit(), "Local", blockB.GasLimit())
+			log.Warn("GasLimit", "A", blockA.GasLimit(), "B", blockB.GasLimit())
 		}
 		if blockA.GasUsed() != blockB.GasUsed() {
-			log.Warn("GasUsed", "Rpc", blockA.GasUsed(), "Local", blockB.GasUsed())
+			log.Warn("GasUsed", "A", blockA.GasUsed(), "B", blockB.GasUsed())
 		}
 		if blockA.Time() != blockB.Time() {
-			log.Warn("Time", "Rpc", blockA.Time(), "Local", blockB.Time())
+			log.Warn("Time", "A", blockA.Time(), "B", blockB.Time())
 		}
 		if blockA.MixDigest() != blockB.MixDigest() {
-			log.Warn("MixDigest", "Rpc", blockA.MixDigest().Hex(), "Local", blockB.MixDigest().Hex())
+			log.Warn("MixDigest", "A", blockA.MixDigest().Hex(), "B", blockB.MixDigest().Hex())
 		}
 		if blockA.Nonce() != blockB.Nonce() {
-			log.Warn("Nonce", "Rpc", blockA.Nonce(), "Local", blockB.Nonce())
+			log.Warn("Nonce", "A", blockA.Nonce(), "B", blockB.Nonce())
 		}
 		if blockA.BaseFee() != blockB.BaseFee() {
-			log.Warn("BaseFee", "Rpc", blockA.BaseFee(), "Local", blockB.BaseFee())
+			log.Warn("BaseFee", "A", blockA.BaseFee(), "B", blockB.BaseFee())
 		}
 
 		for i, txA := range blockA.Transactions() {
